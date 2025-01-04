588ee074841f99be58e6417ad2f11fdcb396562c
Fernando Huerta <fernando.huerta4313@alumnos.udg.mx>
2025-01-03 18:12:00 -0600

    - Seccion de agregar tags en la UI de creacion de ponentes hecha

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,8 +1,39 @@
 (function() {
     const tasgInput = document.querySelector('#tags-input');
-
+    
     if(tasgInput) {
+        const tagsDiv = document.querySelector('#tags');
+        const tagsInputHidden = document.querySelector('[name="tags"]')
         let tags = [];
+
+        const actualizarInputHidden = () => {
+            tagsInputHidden.value = tags.toString();
+        }
+
+        /**
+         * 
+         * @param {Event} e 
+         */
+        const eliminarTag = e => {
+            const tag = e.target.textContent;
+
+            tags = tags.filter(t => t !== tag )
+            e.target.remove();
+            actualizarInputHidden();
+        }
+
+        const mostrarTags = () => {
+            tagsDiv.textContent = '';
+
+            tags.forEach(tag => {
+                const tagLi = document.createElement('LI');
+                tagLi.classList.add('formulario__tag');
+                tagLi.textContent = tag;
+                tagLi.addEventListener('click', eliminarTag);
+                tagsDiv.appendChild(tagLi);
+            })
+        }
+
         /**
          * 
          * @param {Event} e 
@@ -11,11 +42,14 @@
             if(e.keyCode !== 44) 
                 return;
             e.preventDefault();
-            const tag = asgInput.value.trim();
+            const tag = tasgInput.value.trim();
             if(!tag)
                 return
-            else 
+            else {
                 tags = [...tags, tag];
+                mostrarTags();
+                actualizarInputHidden();
+            }
             
             tasgInput.value = '';
         }
